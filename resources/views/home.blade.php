3dfcb5b43d06509c676eeb66d5ccd9db0b520f58
matteomedori <matteo.medori.97@gmail.com>
2024-01-29 16:22:43 +0100

    feat: aggiunto sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Model Controller</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
-    <h1 class="container text-center my-3">Movies</h1>
+    {{-- header --}}
+    <header>
+        <h1 class="container text-center my-3 text-uppercase">Movies</h1>
+        {{-- navbar --}}
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="/">Home</a>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Trending Movies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Tv Series</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn " type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        {{-- /navbar --}}
+    </header>
+    {{-- /header --}}
 
-    <div class="container text-center">
-        <div class="row row-cols-4 gy-4">
-            @foreach ($movies as $movie)
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie['title'] }}</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie['original_title'] }}</h6>
-                            <p class="card-text">Anno d'uscita: {{ Str::substr($movie['date'], 0, 4) }}</p>
-                            <p class="card-text">Votazione: {{ $movie['vote'] }}</p>
+    {{-- main --}}
+    <main>
+        <div class="container text-center">
+            <div class="row row-cols-4 gy-4">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie['title'] }}</h5>
+                                <h6 class="card-subtitle mb-2 ">{{ $movie['original_title'] }}</h6>
+                                <p class="card-text">{{ Str::substr($movie['date'], 0, 4) }}</p>
+                                <p class="card-text">Votazione: {{ $movie['vote'] }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
+    </main>
+    {{-- /main --}}
+
 </body>
 
 </html>
